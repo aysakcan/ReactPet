e91f50ca81a2672879c58eebcf0dcbe7a80c6a48
AYŞE AKCAN <ayse.akcan@netcad.com>
2019-06-27 15:16:31 +0300

    add input clear function

--- src/pages/addapet.js
+++ src/pages/addapet.js
@@ -30,9 +30,20 @@ export default class Homepage extends React.Component {
         };
     }
 
+    inputClear = (e) => {
+        this.setState({
+            name: '',
+            age: '',
+            type: '',
+            genus: '',
+            desc: '',
+            owner: ''
+        });
+    }
+
     onChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
-      }
+    }
 
     onSubmit = (e) => {
         e.preventDefault();
@@ -46,12 +57,12 @@ export default class Homepage extends React.Component {
             genus: genus,
             desc: desc,
             owner: owner
-          };
+        };
 
-          console.log(postData);
+        console.log(postData);
 
         axios.post('http://localhost:3002/insertAPet', postData)
-        .then(window.location.reload()); //this line is horrible
+            .then(window.location.reload()); //this line is horrible
     }
 
     render() {
@@ -69,47 +80,47 @@ export default class Homepage extends React.Component {
                     <div className="row">
                         <div className="col-sm-3"></div>
                         <div className="col">
-                            <Form className="formText" onSubmit={this.onSubmit}>
+                            <Form className="formText">
                                 <FormGroup row>
                                     <Label for="name" sm={3}>Name</Label>
                                     <Col sm={9} className="align-self-center px-4">
-                                        <Input type="text" name="name" id="name" placeholder="Lucy" ref="name" required onChange={this.onChange} value={name}/>
+                                        <Input type="text" name="name" id="name" placeholder="Lucy" ref="name" required onChange={this.onChange} value={name} />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
                                     <Label for="age" sm={3}>Age</Label>
                                     <Col sm={9} className="align-self-center px-4">
-                                        <Input type="number" name="age" id="age" placeholder="2" min="1" ref="age" required onChange={this.onChange} value={age}/>
+                                        <Input type="number" name="age" id="age" placeholder="2" min="1" ref="age" required onChange={this.onChange} value={age} />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
                                     <Label for="type" sm={3}>Type</Label>
                                     <Col sm={9} className="align-self-center px-4">
-                                        <Input type="text" name="type" id="type" placeholder="Dog" ref="type" required onChange={this.onChange} value={type}/>
+                                        <Input type="text" name="type" id="type" placeholder="Dog" ref="type" required onChange={this.onChange} value={type} />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
                                     <Label for="genus" sm={3}>Genus</Label>
                                     <Col sm={9} className="align-self-center px-4">
-                                        <Input type="text" name="genus" id="genus" placeholder="Golden" ref="genus" required onChange={this.onChange} value={genus}/>
+                                        <Input type="text" name="genus" id="genus" placeholder="Golden" ref="genus" required onChange={this.onChange} value={genus} />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
                                     <Label for="desc" sm={3}>Explanation</Label>
                                     <Col sm={9} className="align-self-center px-4">
-                                        <Input type="text" name="desc" id="desc" placeholder="Female.." ref="desc" required onChange={this.onChange} value={desc}/>
+                                        <Input type="text" name="desc" id="desc" placeholder="Female.." ref="desc" required onChange={this.onChange} value={desc} />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
                                     <Label for="owner" sm={3}>Email</Label>
                                     <Col sm={9} className="align-self-center px-4">
-                                        <Input type="email" name="owner" id="owner" placeholder="ownerEmail@example.com" ref="owner" required onChange={this.onChange} value={owner}/>
+                                        <Input type="email" name="owner" id="owner" placeholder="ownerEmail@example.com" ref="owner" required onChange={this.onChange} value={owner} />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup check row>
                                     <Col sm={{ size: 12, offset: 0 }} className="d-flex justify-content-end  px-4">
-                                        <Button className="mr-4 w-25" color="info">Clear</Button>
-                                        <Button color="success w-25">Add</Button>
+                                        <Button className="mr-4 w-25" color="info" onClick={this.inputClear}>Clear</Button>
+                                        <Button color="success w-25" onClick={this.onSubmit}>Add</Button>
                                     </Col>
                                 </FormGroup>
                             </Form>
